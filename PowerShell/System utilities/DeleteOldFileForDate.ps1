abf14c4f8c2c09fc320169f816c1a22078797fe0
KR-sew <lanbak43@gmail.com>
2025-02-14 15:42:55 +0300

    edit DeleteOldFilesForDate.ps1  ✅

--- PowerShell/System utilities/DeleteOldFileForDate.ps1
+++ PowerShell/System utilities/DeleteOldFileForDate.ps1
@@ -1,5 +1,5 @@
 ﻿#####################################################################
-#  Author Карасёв Андрей a.karasew@yandex.ru create date 27.05.2021 #
+#  Author Карасёв Андрей @gmail.com create date 27.05.2021 #
 #####################################################################
 #
 # Скрипт принимает входящие переменные
@@ -9,10 +9,17 @@
 # При вызове через xp_cmdshell нужно оборачивать в переменную
 # командлет Get-Date  в связи с конфликтом с аналогичным по названию в MS QL server (начиная с 2017 и новее)
 ###############################################################################################################
-
-Param ( [string]$DBName, [string]$DBPath )
+# Paremeters: -DBPath the path to database files
+#             -DBName the database name
+#             -Days the amount of days after which backups should be deleted from the current date
+Param ( 
+    [Parameter(Mandatory=$true)]
+    [string]$DBPath, 
+    [int]$Days,
+    [string]$DBName
+    )
 $Path = "$DBPath\$DBName"
-$Days = "-10"
+#$Days = "-10"
 $CurrentDate = Get-Date
 $OldDate = $CurrentDate.AddDays($Days)
 Get-ChildItem $Path -Recurse | Where-Object { $_.LastWriteTime -lt $OldDate } | Remove-Item -Recurse
