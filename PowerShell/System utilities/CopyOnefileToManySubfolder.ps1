a8708f29cca53956e07d8abd7e67c85d1b70b65e
KR-sew <lanbak43@gmail.com>
2025-02-21 17:41:34 +0300

    🔧 [chore]  edit file CopyOneFileToManyFolder.ps1

--- PowerShell/System utilities/CopyOnefileToManySubfolder.ps1
+++ PowerShell/System utilities/CopyOnefileToManySubfolder.ps1
@@ -1,6 +1,28 @@
-$sourceFile = ".\SonarQube\ReadMe.md"
-$destinationFolder = ".\SonarQube"
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$SourceFile,
+    [string]$destinationFolder
+)
 
-Get-ChildItem -Path $destinationFolder -Directory | ForEach-Object {
-    Copy-Item -Path $sourceFile -Destination $_.FullName
+# Define Event Log Source
+$EventSource = "FileCopyScript"
+$EventLogName = "Application"
+
+# Ensure Event Log Source Exists
+if (-not [System.Diagnostics.EventLog]::SourceExists($EventSource)) {
+    New-EventLog -LogName $EventLogName -Source $EventSource
 }
+
+# Copy files and log the operation
+Get-ChildItem -Path $destinationFolder -Directory | ForEach-Object {
+    $DestinationPath = $_.FullName
+
+    # Copy file
+    Copy-Item -Path $SourceFile -Destination $DestinationPath -Force
+
+    # Log the copied file
+    Write-EventLog -LogName $EventLogName -Source $EventSource -EntryType Information -EventId 1003 -Message "Copied file: $SourceFile to $DestinationPath"
+}
+
+# Clear variables
+Clear-Variable SourceFile, destinationFolder
