20eb2a75f3f6f32034e2c54d27fc48e80abd1184
KR-sew <lanbak43@gmail.com>
2025-03-03 16:21:44 +0300

    add a file ➡️

--- Azure/Exams/AZ-104/ChangeVMSize.sh
+++ Azure/Exams/AZ-104/ChangeVMSize.sh
@@ -1,1 +1,3 @@
 az vm list-vm-resize-options --resource-group "[sandbox resource group name]" --name SampleVM --output table
+
+az vm resize --resource-group "[sandbox resource group name]" --name SampleVM --size Standard_D2s_v3
