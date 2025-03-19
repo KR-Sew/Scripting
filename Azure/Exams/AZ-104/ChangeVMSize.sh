53a9330761fa7521f3d16636cfb4dc8691a19c18
KR-sew <lanbak43@gmail.com>
2025-03-19 22:22:14 +0300

    edit ChangeVMSize.sh

--- Azure/Exams/AZ-104/ChangeVMSize.sh
+++ Azure/Exams/AZ-104/ChangeVMSize.sh
@@ -3,12 +3,13 @@
 # Define parameters
 R_GROUP="$1"
 VM_NAME="$2"
+SIZE="$3"
 
 # Validate input
-if [[ -z "$R_GROUP" || -z "$VM_NAME" ]]; then
-    echo "Usage: $0 <resource-group> <vm-name>"
+if [[ -z "$R_GROUP" || -z "$VM_NAME" || -z "$SIZE" ]]; then
+    echo "Usage: $0 <resource-group> <vm-name> <size>"
     exit 1
 fi
 
-# List resizable option of the VM
-az vm list-vm-resize-options --resource-group "$R_GROUP" --name "$VM_NAME" --output table
+# Resize option of the VM
+az vm resize --resource-group "$R_GROUP" --name "$VM_NAME" --size "$SIZE"
