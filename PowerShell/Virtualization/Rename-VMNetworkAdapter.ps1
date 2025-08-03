87e4ad782544ac49a66472ceb2af5f6feec3ff47
KR-sew <lanbak43@gmail.com>
2025-08-03 21:57:21 +0300

    edit Rename-VMNetworkAdapter.ps1

--- PowerShell/Virtualization/Rename-VMNetworkAdapter.ps1
+++ PowerShell/Virtualization/Rename-VMNetworkAdapter.ps1
@@ -1,12 +1,26 @@
-$vmName = "Windows-Dc01"
-$currentAdapterName = "Network Adapter 1"  # Replace with actual adapter name
-$newAdapterName = "Eth01"
 
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$VMName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$currentAdapterName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$newAdapterName
+)
+
+# Ensure the VM is running
+$vm = Get-VM -Name $VMName -ErrorAction Stop
+if ($vm.State -ne 'Running') {
+    Write-Error "VM '$VMName' is not running. Please start it before running this script."
+    exit 1
+}
 try {
     $adapter = Get-VMNetworkAdapter -VMName $vmName -Name $currentAdapterName -ErrorAction Stop
     if ($adapter) {
         $adapter | Rename-VMNetworkAdapter -NewName $newAdapterName -Passthru
-        Write-Output "Successfully renamed network adapter to $newAdapterName for VM $vmName."
+        Write-Output "Successfully renamed network adapter to $newAdapterName for VM $vmName." 
     } else {
         Write-Error "No network adapter found with name $currentAdapterName for VM $vmName."
     }
